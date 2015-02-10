24ca31e85360d6defbf14b2da25d75da764f1dda
R. Biswas <rbiswas4@gmail.com>
2015-02-09 20:43:17 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="SNsims",
+      version="0.0.1dev",
+      description='SNsims produces SN simulations with Ia and core-collapse SN\
+                based on `~sncosmo`',
+      long_description=''' ''',
+      packages=['snsims','tests', 'examples']
+      )
---
+++ snsims/__init__.py
@@ -0,0 +1,2 @@
+from . import snObject
+from . import snUniverse
