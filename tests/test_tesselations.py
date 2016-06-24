99474444e368dfa396cddb56438190d23d379e3c
R. Biswas <rbiswas4@gmail.com>
2016-06-24 16:29:50 -0700

    Added first tests 	modified:   .travis.yml 	new file:   tests/test_tesselations.py

---
+++ tests/test_tesselations.py
@@ -0,0 +1,12 @@
+import snsims
+import pytest
+
+
+def test_tesselations_abc():
+
+    class NotTile(snsims.Tiling):
+        pass
+    with pytest.raises(Exception) as e:
+            t = NotTile()
+    assert e.typename == 'TypeError'
+    assert e.value.message == "Can't instantiate abstract class NotTile with abstract methods __init__, area, pointingSequenceForTile, tileIDSequence, tileIDsForSN"
