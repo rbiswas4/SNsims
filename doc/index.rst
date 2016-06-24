60badf71db12489969d681ce2f7fe466e6c5a3b1
R. Biswas <rbiswas4@gmail.com>
2016-06-23 21:45:15 -0700

    Adding readthedocs files 	new file:   doc/Makefile 	new file:   doc/conf.py 	new file:   doc/index.rst 	modified:   setup.py 	new file:   snsims/__init__.py 	new file:   snsims/version.py

---
+++ doc/index.rst
@@ -0,0 +1,22 @@
+.. snsims documentation master file, created by
+   sphinx-quickstart on Thu Jun 23 21:42:10 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to snsims's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+import sys
+import os
+import re
 
-setup(name="SNsims",
-      version="0.0.1dev",
-      description='SNsims produces SN simulations with Ia and core-collapse SN\
-                based on `~sncosmo`',
+PACKAGENAME = 'snsims'
+packageDir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                          PACKAGENAME)
+versionFile = os.path.join(packageDir, 'version.py')
+
+# Obtain the package version
+with open(versionFile, 'r') as f:
+          s = f.read()
+# Look up the string value assigned to __version__ in version.py using regexp
+versionRegExp = re.compile("__VERSION__ = \"(.*?)\"")
+# Assign to __version__
+__version__ =  versionRegExp.findall(s)[0]
+print(__version__)
+setup(# package information
+      name=PACKAGENAME,
+      version=__version__,
+      description='A package to handle SN simulations over the sky',
       long_description=''' ''',
-      packages=['snsims','tests', 'examples']
+      # What code to include as packages
+      packages=[PACKAGENAME],
+      packagedir={PACKAGENAME: 'snsims'},
+      # What data to include as packages
+      include_package_data=True,
+      package_data={PACKAGENAME:['example_data/*']}
       )
