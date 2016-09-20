18dff17ce72d342871d4503df212f42825373872
R. Biswas <rbiswas4@gmail.com>
2016-09-20 15:22:24 -0700

    Added further install requirements of analyzeSN 	modified:   continuous_integration/install_analyzeSN.sh

--- continuous_integration/install_analyzeSN.sh
+++ continuous_integration/install_analyzeSN.sh
@@ -3,5 +3,8 @@ pd=$PWD
 cd ..
 git clone https://github.com/rbiswas4/AnalyzeSN.git
 cd AnalyzeSN
+./continuous_integration/travis_install.sh
+source ./continuous_integration/setup_env.sh
+pip install -r ./continuous_integration/pip_requirements.txt
 python setup.py install --user
 cd $pd
