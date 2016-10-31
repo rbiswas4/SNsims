62b14044a6238ddfc30debb31fbbaff139cc9503
R. Biswas <rbiswas4@gmail.com>
2016-10-31 01:04:36 -0700

    Added fitsio install 	modified:   continuous_integration/install_opsimsummary.sh

--- continuous_integration/install_opsimsummary.sh
+++ continuous_integration/install_opsimsummary.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 pd=$PWD
 cd ..
+fit clone https://github.com/esheldon/fitsio.git
+cd fitsio
+pyton setup.py install --user
+cd ..
 git clone https://github.com/rbiswas4/OpSimSummary.git
 cd OpSimSummary
 python setup.py install --user
