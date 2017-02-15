1d4c22d76abdb1f5f753663a29aed7e3b59a717e
R. Biswas <rbiswas4@gmail.com>
2017-02-14 22:21:49 -0800

    removed fieldID

--- snsims/simulations.py
+++ snsims/simulations.py
@@ -375,7 +375,7 @@ class SimulationTile(Universe):
         df['ModelFlux'] = modelFlux
         df['flux'] = df['ModelFlux'] + df['deviations'] * df['fluxerr']
         df['zpsys']= 'ab'
-        lc = df[['snid', 'expMJD', 'filter', 'ModelFlux', 'fieldID', 'flux', 'fluxerr',
+        lc = df[['snid', 'expMJD', 'filter', 'ModelFlux', 'flux', 'fluxerr',
                  'zp', 'zpsys', 'fieldID']]
         return LightCurve(lc, bandNameDict=simBandNameDict)
 
