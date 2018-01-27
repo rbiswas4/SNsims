f25f07d610826fec7a31075d1ab3b5e6f4649d9d
R. Biswas <rbiswas4@gmail.com>
2018-01-27 15:47:25 +0100

    Fixed changed api of pytest in python3. change works on both py2&3 	modified:   tests/test_tesselations.py

--- tests/test_tesselations.py
+++ tests/test_tesselations.py
@@ -9,4 +9,4 @@ def test_tesselations_abc():
     with pytest.raises(Exception) as e:
             t = NotTile()
     assert e.typename == 'TypeError'
-    assert e.value.message == "Can't instantiate abstract class NotTile with abstract methods __init__, area, pointingSequenceForTile, positions, tileIDSequence, tileIDsForSN"
+    assert e.value.__str__() == "Can't instantiate abstract class NotTile with abstract methods __init__, area, pointingSequenceForTile, positions, tileIDSequence, tileIDsForSN"
